ca83f3ab1a7eb52cdcd02ab92f14e51c7b64d283
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-20 16:12:33 +0400

    Added pins and info text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first AGM project';
+  // Zoom Level
+  zoom = 8;
+
+  // Start Position
   lat = 41.7151;
   lng = 44.8271;
+  markers: marker[] = [
+    {
+      name: 'Tbilisi',
+      lat: 41.7151,
+      lng: 44.8271,
+      draggable: true
+    },
+    {
+      name: 'Kutaisi',
+      lat: 42.26791,
+      lng: 42.69459,
+      draggable: true
+    },
+    {
+      name: 'Batumi',
+      lat: 41.6168,
+      lng: 41.6367,
+      draggable: true
+    }
+  ];
+
+  constructor() {
+
+  }
+}
+
+// Marker Type
+interface marker {
+  name?: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
